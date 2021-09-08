589c7edade7de57a0d11a0230db1cf739f40553c
Xiangyi  Luo <u6162693@anu.edu.au>
2021-09-08 22:18:15 +0800

    add golden cross

---
+++ Strategies/GoldenCrossStrategy.py
@@ -0,0 +1,39 @@
+import math
+import backtrader as bt
+
+
+class GoldenCrossStrategy(bt.Strategy):
+
+    params = (('fast', 7), ('slow', 21), ('order_percentage', 0.80), ('ticker', 'AAPL'))
+
+    def __init__(self):
+
+        self.fast_moving_average = bt.indicators.SMA(
+            self.data.close, period=self.params.fast, plotname='7 day moving average'
+         )
+        self.slow_moving_average = bt.indicators.SMA(
+            self.data.close, period=self.params.slow, plotname='30 day moving average'
+        )
+
+        self.crossover = bt.indicators.CrossOver(self.fast_moving_average, self.slow_moving_average)
+
+
+    def next(self):
+
+        if self.position.size == 0 and self.crossover > 0:
+
+            amount_to_invest = (self.params.order_percentage * self.broker.cash)
+
+            self.size = math.floor(amount_to_invest / self.data.close)
+
+            print('BUY {} shares of {} at {}'.format(self.size, self.params.ticker, self.data.close[0]))
+
+            self.buy(size=self.size)
+
+        elif self.position.size > 0 > self.crossover:
+
+            print('SELL {} shares of {} at {}'.format(self.size, self.params.ticker, self.data.close[0]))
+            self.close()
+
+
+
--- Trader.py
+++ Trader.py
@@ -1,9 +1,8 @@
 import datetime  # For datetime objects
-import os.path  # To manage paths
-import sys  # To find out the script name (in argv[0])
 
 import backtrader as bt
-from FirstStrategy import TestStrategy
+from Strategies.FirstStrategy import TestStrategy
+from Strategies.GoldenCrossStrategy import GoldenCrossStrategy
 
 
 cerebro = bt.Cerebro()
@@ -11,24 +10,27 @@ cerebro.broker.set_cash(1000000)
 print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
 # ########################
-AAPL_HISTORY = 'aapl.csv'
+AAPL_HISTORY = '/Users/akira/PycharmProjects/AkiraIsTrading/stocks_history_data/aapl.csv'
 
 
 # Create a Data Feed
 data = bt.feeds.YahooFinanceCSVData(
     dataname=AAPL_HISTORY,
     # Do not pass values before this date
-    # fromdate=datetime.datetime(2000, 1, 1),
+    fromdate=datetime.datetime(2021, 1, 1),
     # Do not pass values after this date
-    # todate=datetime.datetime(2000, 12, 31),
+    todate=datetime.datetime(2021, 12, 31),
     reverse=False)
 
+
+# ibstore = bt.stores.IBStore(host='127.0.0.1', port=7496, clientId=35)
+# data = ibstore.getdata(dataname='EUR.USD-CASH-IDEALPRO')
 
 cerebro.addsizer(bt.sizers.FixedSize, stake=1000)
 
 cerebro.adddata(data)
 
-cerebro.addstrategy(TestStrategy)
+cerebro.addstrategy(GoldenCrossStrategy)
 
 cerebro.run()
 
